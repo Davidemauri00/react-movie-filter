1890cafee1595d6fbaf5c165f697650201840d30
Davidemauri00 <dadi.spyro@gmail.com>
2026-03-01 13:01:28 +0100

    Start

---
+++ react-movie-filter/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+
+const films = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
